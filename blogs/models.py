3ca31f136531b199b9712d7ae8ad4134bccb2191
tyw809611051 <809611051@qq.com>
2019-04-12 13:57:30 +0800

    迁移数据库

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Employee(models.Model):
-    name = models.CharField(max_length=20)
+class Article(models.Model):
+    title   = models.CharField(max_length=255)
+    picture = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    excerpt = models.CharField(max_length=255)
+    author  = models.CharField(max_length=125)
+    users_id= models.IntegerField(default=0)
+    tag     = models.CharField(max_length=100)
+    status  = models.IntegerField(default=1)
+    category_id = models.IntegerField(default=0)
+    created_at  = models.DateTimeField('date created_at')
+    updated_at  = models.DateTimeField('date updated_at')
