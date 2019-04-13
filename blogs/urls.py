d3e1a0087f8d02a33733ba15ad897b3f5101233c
tyw809611051 <809611051@qq.com>
2019-04-13 16:20:20 +0800

    view

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    # e%: /blogs/5
+    path('article/<int:article_id>/', views.detail, name='detail'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
+from django.template import loader
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello world")
+
+def detail(request, article_id):
+    list = Article.objects.get(id=1)
+    template = loader.get_template('blogs/index.html')
+    context = {'list':list}
+    return HttpResponse(template.render(context,request))
+
