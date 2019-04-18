c15a68a568727b953976450f525b3b59f8790736
tyw809611051 <809611051@qq.com>
2019-04-18 14:14:20 +0800

    首页

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index,name='index'),
     # e%: /blogs/5
     path('article/<int:article_id>/', views.detail, name='detail'),
+    path('article/add', views.add, name='add'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -5,11 +5,14 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    return render(request, 'blogs/index.html')
+
+def add(request):
+    return render(request, 'blogs/index.html')
 
 def detail(request, article_id):
     list = Article.objects.get(id=1)
     template = loader.get_template('blogs/index.html')
     context = {'list':list}
-    return HttpResponse(template.render(context,request))
+    return render(request, 'blogs/index.html')
 
