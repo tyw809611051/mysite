9feb44ae4cfc7ef3904f53d3bc36cb4363802b55
tyw809611051 <809611051@qq.com>
2019-04-18 14:40:08 +0800

    调整路由

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name="blogs"
 urlpatterns = [
     path('', views.index,name='index'),
     # e%: /blogs/5
-    path('article/<int:article_id>/', views.detail, name='detail'),
-    path('article/add', views.add, name='add'),
+    path('article',include([
+        path('',views.index),
+        path('add',views.add),
+        path('edit/<int:article_id>',views.edit),
+        path('delete/<int:article_id>',views.delete),
+    ])),
 ]
